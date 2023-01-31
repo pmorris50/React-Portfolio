c47a6e6bfaf5c36eb5e97967d3f897a7404d3918
pmorris50 <patrickmorris532@gmail.com>
2023-01-30 21:15:21 -0700

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Card from './components/Project';
+import Footer from './components/Footer'
 import './App.css';
+import {send} from 'emailjs-com';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'container'>
+      <Header/>
+      {/* <Navbar/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Footer/> */}
     </div>
   );
 }
---
+++ src/components/ContactForm.js
@@ -0,0 +1,41 @@
+import { send } from 'emailjs-com';
+
+function ContactForm(props) {
+
+
+
+return (
+<div className ="ContactForm">
+<form onSubmit={onSubmit}>
+  <input
+    type='text'
+    name='from_name'
+    placeholder='from name'
+    value={toSend.from_name}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='to_name'
+    placeholder='to name'
+    value={toSend.to_name}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='message'
+    placeholder='Your message'
+    value={toSend.message}
+    onChange={handleChange}
+  />
+  <input
+    type='text'
+    name='reply_to'
+    placeholder='Your email'
+    value={toSend.reply_to}
+    onChange={handleChange}
+  />
+  <button type='submit'>Submit</button>
+</form>
+</div>
+)}
