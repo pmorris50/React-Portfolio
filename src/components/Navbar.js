5d486d56554427c3fa736a3add2cf56ba1cdc017
pmorris50 <patrickmorris532@gmail.com>
2023-01-31 19:38:37 -0700

    all pages are rendering. styling is not showing up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
-const NavigationBar = () => (
+const NavigationBar = (props) => (
   <Navbar bg="light" expand="lg">
     <Container>
       <Navbar.Brand href="#home">Patrick Morris</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+        <Nav.Link onClick={() => props.handlePageChange('Home')}>Home</Nav.Link>
+          <Nav.Link onClick={() => props.handlePageChange('About')}>About</Nav.Link>
+          <Nav.Link onClick={() => props.handlePageChange('Contact')}>Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
