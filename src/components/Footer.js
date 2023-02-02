5b1e59312ddc3304635688085702831aca3f4b67
pmorris50 <patrickmorris532@gmail.com>
2023-02-01 20:58:22 -0700

    format form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
-import {FaGithubSquare} from 'react-icons/fa'; 
-import {BsLinkedin} from 'react-icons/bs';
-import {IconContext} from 'react-icons';
-import {FcIphone} from 'react-icons/fc';
+import { FaGithubSquare } from 'react-icons/fa';
+import { BsLinkedin } from 'react-icons/bs';
+import { IconContext } from 'react-icons';
+import { FcIphone } from 'react-icons/fc';
+import {MdOutlineEmail} from 'react-icons/md';
 // import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => (
   <Navbar bg="dark" variant="dark" fixed="bottom">
     <Container>
-      <Row className = 'd-flex'>
-        <Col md={12} className = 'justify-content-center'> 
-          <Nav className="mr-auto">
-            <IconContext.Provider className = 'text-center' value = {{color: '#C0C0C0', size: 42}}>
-            <Nav.Link href="#home"> <FaGithubSquare/></Nav.Link>
-            <Nav.Link href="#about"><BsLinkedin/></Nav.Link>
-            <Nav.Link href="#contact"><FcIphone/></Nav.Link>
+      <Row className='justify-content-center'>
+        <Col md={12} className=''>
+          <Nav className="mr-auto" >
+            <IconContext.Provider value={{ color: '#C0C0C0', size: 42 }}>
+              <div className='d-flex justify-content-center'>
+                  <Nav.Link target="_blank" href="https://github.com/pmorris50"> <FaGithubSquare /></Nav.Link>
+                  <Nav.Link target="_blank" href="https://www.linkedin.com/in/patrick-morris-b656b8197/"><BsLinkedin /></Nav.Link>
+                  <Nav.Link href="tel: +1-940-367-7687"><FcIphone /></Nav.Link>
+                  <Nav.Link target="_blank" href="mailto: patrickmorris532@gmail.com"> <MdOutlineEmail /></Nav.Link>
+               </div>
+
             </IconContext.Provider>
-            {/* <Nav.Link href="https://github.com/pmorris50">
-              <FaGithub size={20} className="mr-2" />
-              GitHub
-            </Nav.Link> */}
+
           </Nav>
         </Col>
       </Row>
-      
-        <Col md={4}>
-          <p className="text-white text-center">Copyright &copy; 2023</p>
-        </Col>
     </Container>
   </Navbar>
 );
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,182 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TimePicker from 'react-time-picker';
+import DatePicker from 'react-date-picker';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
-export default function Contact(){
-    return(
-        <div>
-            <h1>Contact Me</h1>
-        </div>
-    );
-}
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+    date: '',
+    time: '',
+
+  });
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState('00:00');
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form Data: ', formData);
+    // You would add the logic to send the form data to your backend here
+  };
+
+  return (
+    <Container className="my-5">
+      <Row className="justify-content-center">
+        <Col md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formSubject">
+              <Form.Label>Subject</Form.Label>
+              <Form.Control
+                type="text"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="3"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <div className ='mt-2'>
+            <Form.Group controlId="formDate">
+              <Form.Label>Date:</Form.Label>
+              <DatePicker
+                onChange={date => setSelectedDate(date)}
+                value={selectedDate}
+              />
+            </Form.Group>
+            </div>
+              <div className ='mt-2'>
+            <Form.Group controlId="formTime">
+              <Form.Label>Time:</Form.Label>
+              <TimePicker disableClock = {true}
+                onChange={time => setSelectedTime(time)}
+                value={selectedTime}
+              />
+            </Form.Group>
+            </div>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ContactForm;
+
+
+
+// const Form = () => {
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     subject: '',
+//     message: '',
+//     date: '',
+//     time: ''
+//   });
+
+//   const handleChange = (event) => {
+//     setFormData({
+//       ...formData,
+//       [event.target.name]: event.target.value
+//     });
+//   };
+
+//   return (
+//     <form>
+//       <div>
+//         <label htmlFor="email">Email:</label>
+//         <input
+//           type="email"
+//           id="email"
+//           name="email"
+//           value={formData.email}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="subject">Subject:</label>
+//         <input
+//           type="text"
+//           id="subject"
+//           name="subject"
+//           value={formData.subject}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="message">Message:</label>
+//         <textarea
+//           id="message"
+//           name="message"
+//           value={formData.message}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="date">Date:</label>
+//         <input
+//           type="date"
+//           id="date"
+//           name="date"
+//           value={formData.date}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="time">Time:</label>
+//         <input
+//           type="time"
+//           id="time"
+//           name="time"
+//           value={formData.time}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// };
+
+// export default Form;
