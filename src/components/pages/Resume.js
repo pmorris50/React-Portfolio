e469a573de0ee434568fbe43f2bd4f5ee9d4b26c
pmorris50 <patrickmorris532@gmail.com>
2023-02-05 07:42:03 -0700

    saving

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,6 +8,14 @@ function Resume() {
         background:
         {
             background: '#B3B4B5'
+        },
+        jobtext: {
+            fontSize: '20px',
+            fontWeight: 'bold'
+        },
+        timetext: {
+            textDecoration: 'underline'
+
         }
     }
     return (
@@ -23,7 +31,7 @@ function Resume() {
       <div className="col-md-9">
         <h1>Patrick Morris</h1>
         <h5>Web Developer</h5>
-        <p className="border-top pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio lacus, sollicitudin in dolor at, consequat volutpat ante. Integer quis consequat turpis, quis porta orci. Proin tincidunt volutpat faucibus. Suspendisse ac nisl purus suspendisse eleifend interdum orci non pharetra. </p>
+        <p className="border-top pt-3">Thank you for visiting my Portfolio. You can take a look at my resume below and download a PDF version </p>
       </div>       
     </div>        
   </div>
@@ -32,27 +40,89 @@ function Resume() {
   <div class="row">
     <div class="col-md mb-5">
      <h3>Work Experience</h3>
-     <ul>
+     <ul style={style.jobtext}>
        Pulte Mortgage Loan Consultant
         </ul> 
-        <li>
+        <li style = {style.timetext}>
          From December 2021 - October 2023
         </li>
         <li>
         I was actively taking loan applications and fielding a back log of 50 plus customers at all times
         </li>
+        <li>
+           A customer facing role where excellent customer service was the standard.
+        </li>
+        <ul style={style.jobtext} className = 'mt-3'>
+       NFL Player
+        </ul> 
+        <li style ={style.timetext}>
+         From May 2018 - July 2021
+        </li>
+        <li>
+      Position: Center
+        </li>
+        <li>
+        Duties: Full control of controlling the offensive line blocking schemes in accordance to team philosophy
+        </li>
+        <li>
+            Quick and confident decision making is mandetory for this position
+        </li>
+        <li>
+            Understanding team cohesiveness and dedication is more important than individual talent. 
+        </li>
+        <ul style={style.jobtext} className = 'mt-3'>
+      KPMG Tax intern
+        </ul> 
+        <li style ={style.timetext}>
+         From January 2017 - March 2017
+        </li>
+        <li>
+      
+        </li>
+        <li>
+        Duties: calculating taxable income, and filling out respective IRS forms. As well as planning taxable events
+        </li>
+        <li>
+        Attention to detail and learning new systems and regulations were major components of this job
+        </li>
+        <li>
+           I was on track to complete my masters in accounting at TCU and return full time but ultimately I chose to pursue a career in football
+        </li>
+        
     </div>
     <div class="col-md mb-5">
      <h3>Education</h3>
+     <ul style={style.jobtext} className = 'mt-3'>
+      TCU
+        </ul> 
+        <li style ={style.timetext}>
+         From June 2013 - December 2017
+        </li>
+        <li>
+      B.B.A in Accounting: Cum Laude
+        </li>
+        <ul style={style.jobtext} className = 'mt-3'>
+      Rutgers
+        </ul> 
+        <li style ={style.timetext}>
+         From November 2022 - February 2023
+        </li>
+        <li>
+      Fullstack web developer bootcamp
+        </li>
+        <ul style={style.jobtext} className = 'mt-3'>
+      Skills
+        </ul> 
+        <li>
+        Licensed Mortgage Loan Originator NMLS #2223246 
+        </li>    
+
+     
 
     </div>     
   </div>    
-  <div class="row">
-    <div class="col-md mb-5">
-      <h3>Languages</h3>
-    </div>
     
-  </div>
+
 </main>
 
 </Container>
